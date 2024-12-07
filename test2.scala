5493fd59d32d6872c33e49566f4436cafd819441
sreesaikiran <sreesaikiran.91@gmail.com>
2024-12-07 13:52:53 -0500

    Update test2.scala

--- test2.scala
+++ test2.scala
@@ -1,103 +1,182 @@
+package com.zaplabs
+
+import com.typesafe.config.Config
+import com.zaplabs.client.kafka.MdpKafkaProducer
+import com.zaplabs.helper.listing.KafkaHelper
+import com.zaplabs.utils.ConfigUtils
+import org.apache.avro.Schema
 import org.apache.spark.sql.{DataFrame, Row}
-import org.mongodb.scala._
-import org.mongodb.scala.model.Filters
-import org.mongodb.scala.model.Updates
-import org.mongodb.scala.bson.BsonDateTime
-import scala.collection.mutable.ArrayBuffer
-import scala.util.{Failure, Success}
+import org.mongodb.scala.bson.collection.immutable.Document
+import org.mongodb.scala.bson.{BsonArray, BsonBoolean, BsonDateTime, BsonDocument, BsonDouble, BsonInt32, BsonString}
+import org.mongodb.scala.model.{Filters, Updates}
+import org.mongodb.scala.{MongoClient, _}
+
+import java.time.{Instant, LocalDateTime, ZoneOffset}
+import scala.collection.JavaConverters._
+import scala.collection.mutable.WrappedArray
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.chaining.scalaUtilChainingOps
+import scala.util.{Failure, Success, Try}
 
 object OpenHomesProcessor {
 
   def processPartition(df: DataFrame, config: Config, now: LocalDateTime, mlsName: String, batchSize: Int): Unit = {
-    df.foreachPartition { partition =>
-      // Create MongoClient inside foreachPartition
+    df.foreachPartition { partition: Iterator[Row] =>
       val mongoClient: MongoClient = MongoClient(config.getString("mongodb.connectionString"))
       val database: MongoDatabase = mongoClient.getDatabase(config.getString("mongodb.database"))
       val collection: MongoCollection[Document] = database.getCollection(config.getString("mongodb.activeCollection"))
 
-      val messagesBuffer = ArrayBuffer[Document]()
+      val startTime = Instant.now()
+      val kafkaTopic = config.getString("publishJob.kafkaTopic")
+      val kafkaClientId = config.getString("kafkaConnectorConfiguration.clientName")
+      val kafkaConfig = new java.util.Properties()
+          .tap(_.putAll(ConfigUtils.flatten(config.getObject("kafkaConnectorConfiguration")).asJava))
+      val kafkaSchemaPath = config.getString("publishJob.schemaPath")
+      val clazz = getClass
+      val schemaPath = clazz.getResourceAsStream(kafkaSchemaPath)
+      val kafkaProducer = new MdpKafkaProducer(
+        kafkaTopic,
+        kafkaClientId,
+        kafkaConfig,
+        new Schema.Parser().parse(schemaPath)
+      )
+      // Buffer to hold Kafka messages
+      val messagesBuffer = scala.collection.mutable.ArrayBuffer[Document]()
 
       try {
         partition.foreach { row =>
-          val id = row.getAs[String]("_id")
-          if (id == null || id.isEmpty) {
-            println(s"Skipping row with missing or invalid _id: $row")
-          } else {
-            val nowMillis = now.toInstant(ZoneOffset.UTC).toEpochMilli
+          val nowMillis = now.toInstant(ZoneOffset.UTC).toEpochMilli
 
-            // Example processing logic for openHomes
-            val openHomes = Option(row.getAs[Seq[Row]]("open_homes")).getOrElse(Seq.empty).filter { openHome =>
-              extractTimestamp(openHome.getAs[Any]("open_house_start_time")).exists(_ >= nowMillis)
-            }
+          // Extract and filter open homes
+          val openHomes = Option(row.getAs[Seq[Row]]("open_homes")).getOrElse(Seq.empty).filter { openHome =>
+            extractTimestamp(openHome.getAs[Any]("open_house_start_time")).exists(_ >= nowMillis)
+          }
 
-            val processingDate = BsonDateTime(System.currentTimeMillis())
-            val filter = Filters.eq("_id", id)
+          val processingDate = new BsonDateTime(System.currentTimeMillis())
+          val filter = Filters.eq("_id", row.getAs[String]("_id"))
 
-            val update = if (openHomes.nonEmpty) {
-              val openHomesHash = openHomes.map(_.hashCode()).sorted.hashCode()
-              Updates.combine(
-                Updates.set("open_house.open_homes", openHomes.map(_.toString)),
-                Updates.set("open_house.hash_code", openHomesHash),
-                Updates.set("last_change_date", processingDate)
-              )
-            } else {
-              Updates.combine(
-                Updates.set("open_house.open_homes", Seq.empty[String]),
-                Updates.set("open_house.hash_code", 0),
-                Updates.set("last_change_date", processingDate)
-              )
-            }
+          val update = if (openHomes.nonEmpty) {
+            val openHomesHash = openHomes
+              .map(entry => (
+                extractTimestamp(entry.getAs[Any]("open_house_date")).getOrElse(Long.MaxValue),
+                extractTimestamp(entry.getAs[Any]("open_house_start_time")).getOrElse(Long.MaxValue),
+                extractTimestamp(entry.getAs[Any]("open_house_end_time")).getOrElse(Long.MaxValue),
+                entry.getAs[Boolean]("is_canceled")
+              ))
+              .sortBy(_.hashCode())
+              .hashCode()
 
-            // Perform the update operation
-            val updateFuture = collection.updateOne(filter, update).toFuture()
-            updateFuture.onComplete {
-              case Success(_) =>
-                // Retrieve and add the updated document to buffer
-                val findFuture = collection.find(filter).first().toFuture()
-                findFuture.onComplete {
-                  case Success(updatedDocument) =>
-                    messagesBuffer += updatedDocument
-                    if (messagesBuffer.size >= batchSize) {
-                      sendBatchToKafka(messagesBuffer, mlsName)
-                      messagesBuffer.clear()
-                    }
-                  case Failure(ex) =>
-                    println(s"Failed to retrieve updated document with _id $id: ${ex.getMessage}")
+            val openHouseArray = openHomes.map { openHome =>
+              val fields = openHome.schema.fieldNames.map { fieldName =>
+                val value = openHome.getAs[Any](fieldName) match {
+                  case s: String => BsonString(s)
+                  case i: Int => BsonInt32(i)
+                  case b: Boolean => BsonBoolean(b)
+                  case d: Double => BsonDouble(d)
+                  case ts: java.sql.Timestamp => BsonDateTime(ts.getTime)
+                  case arr: WrappedArray[_] => BsonArray(arr.map {
+                    case s: String => BsonString(s)
+                    case i: Int => BsonInt32(i)
+                    case b: Boolean => BsonBoolean(b)
+                    case d: Double => BsonDouble(d)
+                    case ts: java.sql.Timestamp => BsonDateTime(ts.getTime)
+                    case other => throw new IllegalArgumentException(s"Unsupported type in array: ${other.getClass}")
+                  }.toSeq.asJava)
+                  case other => throw new IllegalArgumentException(s"Unsupported type: ${other.getClass}")
                 }
-              case Failure(ex) =>
-                println(s"Failed to update document with _id $id: ${ex.getMessage}")
+                fieldName -> value
+              }.toMap
+              Document(BsonDocument(fields))
             }
+
+            Updates.combine(
+              Updates.set("open_house.open_homes", openHouseArray),
+              Updates.set("open_house.hash_code", openHomesHash),
+              Updates.set("last_change_date", processingDate),
+              Updates.set("property.listing.dates.last_change_date", processingDate)
+            )
+          } else {
+            Updates.combine(
+              Updates.set("open_house.open_homes", Seq.empty.asJava),
+              Updates.set("open_house.hash_code", 0),
+              Updates.set("open_house.is_open_homes", false),
+              Updates.set("last_change_date", processingDate),
+              Updates.set("property.listing.dates.last_change_date", processingDate)
+            )
+          }
+
+          // Perform the update asynchronously
+          val updateFuture = collection.updateOne(filter, update).toFuture()
+          updateFuture.onComplete {
+            case Success(_) =>
+              // Retrieve the updated document & add to buffer
+              val findFuture = collection.find(filter).first().toFuture()
+              findFuture.onComplete {
+                case Success(updatedDocument) =>
+                  messagesBuffer += updatedDocument
+                  if (messagesBuffer.size >= batchSize) {
+                    sendBatchToKafka(kafkaProducer, messagesBuffer, mlsName, startTime)
+                    messagesBuffer.clear()
+                  }
+                case Failure(ex) =>
+                  println(s"Failed to retrieve updated document with _id ${row.getAs[String]("_id")}: ${ex.getMessage}")
+              }
+            case Failure(ex) =>
+              println(s"Failed to update document with _id ${row.getAs[String]("_id")}: ${ex.getMessage}")
           }
         }
 
         // Send remaining messages in the buffer
         if (messagesBuffer.nonEmpty) {
-          sendBatchToKafka(messagesBuffer, mlsName)
+          sendBatchToKafka(kafkaProducer, messagesBuffer, mlsName, startTime)
           messagesBuffer.clear()
         }
+
       } finally {
+
+        kafkaProducer.flush()
+        kafkaProducer.close()
         mongoClient.close()
       }
     }
+
   }
 
-  private def sendBatchToKafka(messagesBuffer: ArrayBuffer[Document], mlsName: String): Unit = {
-    println(s"Sending ${messagesBuffer.size} messages to Kafka for MLS: $mlsName.")
-    messagesBuffer.foreach { doc =>
-      println(doc.toJson) // Replace with Kafka producer logic
-    }
-  }
-
-  private def extractTimestamp(value: Any): Option[Long] = {
-    try {
-      value match {
-        case ts: java.sql.Timestamp => Some(ts.getTime)
-        case s: String              => Some(java.sql.Timestamp.valueOf(s).getTime)
-        case l: Long                => Some(l)
-        case _                      => None
+  /**
+   * Utility method to send a batch of messages to Kafka.
+   */
+  private def sendBatchToKafka(kafkaProducer: MdpKafkaProducer, messagesBuffer: Seq[Document], mlsName:String, startTime:Instant): Unit = {
+    messagesBuffer.foreach { message =>
+      Try{
+        val processingTs = Instant.now()
+        KafkaHelper.sendMessageToKafkaSecondaryUpdates(
+          message,
+          mlsName,
+          "default_job_id",
+          "open_homes",
+          processingTs,
+          startTime.toEpochMilli,
+          kafkaProducer
+        )
+      }.recover {
+        case ex: Exception =>
+          println(s"Failed to send document ID: ${message.get("_id")}. Error: ${ex.getMessage}")
       }
-    } catch {
-      case _: Throwable => None
     }
+    println(s"Sent batch of ${messagesBuffer.size} messages to Kafka.")
+  }
+
+  /**
+   * Utility method to extract timestamps.
+   */
+  private def extractTimestamp(value: Any): Option[Long] = {
+    Try {
+      value match {
+        case ts: java.sql.Timestamp => ts.getTime
+        case s: String              => java.sql.Timestamp.valueOf(s).getTime
+        case l: Long                => l
+        case _                      => throw new IllegalArgumentException("Invalid timestamp type")
+      }
+    }.toOption
   }
 }
